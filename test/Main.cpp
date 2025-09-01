b9f30015b56fa111ec26d4ed7290239449e79c32
adellafrattina <adellafrattina@studenti.uninsubria.it>
2025-09-01 09:32:45 +0200

    Added graph data creation

--- test/Main.cpp
+++ test/Main.cpp
@@ -1,21 +1,149 @@
 #include <iostream>
+#include <fstream>
+#include <chrono>
 
 #include <BigInteger.hpp>
 
+class Timer {
+
+public:
+
+	Timer() {
+
+		start_time = std::chrono::high_resolution_clock::now();
+	}
+
+	void reset() {
+
+		start_time = std::chrono::high_resolution_clock::now();
+	}
+
+	double elapsedNanoseconds() const {
+
+		auto end_time = std::chrono::high_resolution_clock::now();
+		std::chrono::duration<double, std::nano> duration = end_time - start_time;
+		return duration.count();
+	}
+
+	double elapsedMicroseconds() const {
+
+		auto end_time = std::chrono::high_resolution_clock::now();
+		std::chrono::duration<double, std::micro> duration = end_time - start_time;
+		return duration.count();
+	}
+
+	double elapsedMilliseconds() const {
+
+		auto end_time = std::chrono::high_resolution_clock::now();
+		std::chrono::duration<double, std::milli> duration = end_time - start_time;
+		return duration.count();
+	}
+
+	double elapsedSeconds() const {
+
+		auto end_time = std::chrono::high_resolution_clock::now();
+		std::chrono::duration<double> duration = end_time - start_time;
+		return duration.count();
+	}
+
+private:
+
+	std::chrono::high_resolution_clock::time_point start_time;
+};
+
+static void factorial(std::size_t n) {
+
+	big::Integer r(1);
+	for (std::size_t i = 1; i <= n; i++)
+		r *= i;
+}
+
+static std::ofstream fout;
+static std::size_t bytes = 0;
+
+void* operator new(std::size_t size) {
+
+	bytes += size;
+	void* p = std::malloc(size);
+	if (!p) throw std::bad_alloc();
+
+	return p;
+}
+
+static void DumpSumInfo(std::size_t bits) {
+
+	big::Integer a(0, bits);
+	big::Integer::Rand(a);
+	big::Integer b(0, bits);
+	big::Integer::Rand(b);
+	std::cout << "(" << bits << " + " << bits << ")...";
+	Timer timer;
+	bytes = 0;
+	a + b;
+	double time = timer.elapsedMicroseconds();
+	fout << bits << ";" << time << ";" << bytes << "\n";
+	fout.flush();
+	std::cout << " Done!\n";
+}
+
+static void DumpMultInfo(std::size_t bits) {
+
+	big::Integer a(0, bits);
+	big::Integer::Rand(a);
+	big::Integer b(0, bits);
+	big::Integer::Rand(b);
+	std::cout << "(" << bits << " * " << bits << ")...";
+	Timer timer;
+	bytes = 0;
+	a * b;
+	double time = timer.elapsedMicroseconds();
+	fout << bits << ";" << time << ";" << bytes << "\n";
+	fout.flush();
+	std::cout << " Done!\n";
+}
+
+static void DumpFactInfo(std::size_t bits) {
+
+	std::cout << "factorial(" << bits << ")...";
+	Timer timer;
+	bytes = 0;
+	factorial(bits);
+	double time = timer.elapsedMicroseconds();
+	fout << bits << ";" << time << ";" << bytes << "\n";
+	fout.flush();
+	std::cout << " Done!\n";
+}
+
 int main() {
 
-	// 18446744073709551615
-	// 9223372036854775807
+	// Sum
+	std::cout << "--- Sum data ---\n";
+	fout.open("data-sum.csv");
+	if (!fout.is_open())
+		return -1;
 
-	PRINT("Test %d", 1);
-	big::Integer n1(0);
-	big::Integer n2(0);
-	std::cin >> n1 >> n2;
-	if (std::cin.fail())
-		std::cerr << "ERROR\n";
-	else
-		std::cout << n1 + n2 << "\n";
-	//std::cout << n1 << " (Size: " << n1.Size() << " - Capacity: " << n1.Capacity() << ")\n";
+	for (std::size_t i = 1; i <= 4096; i++)
+		DumpSumInfo(i * 64);
 
-	return 0;
+	// Multiplication
+	fout.close();
+	std::cout << "--- Multiplication data ---\n";
+	fout.open("data-mult.csv");
+	if (!fout.is_open())
+		return -1;
+
+	for (std::size_t i = 1; i <= 4096; i++)
+		DumpMultInfo(i * 64);
+
+	// Factorial
+	std::cout << "--- Factorial data ---\n";
+	fout.close();
+	fout.open("data-fact.csv");
+	if (!fout.is_open())
+		return -1;
+
+	for (std::size_t i = 1; i <= 1000; i++)
+		DumpFactInfo(i);
+
+	fout.close();
 }
