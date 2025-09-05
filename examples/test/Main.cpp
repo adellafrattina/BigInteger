b6a5433fa1ac3f4f6072b581451c25e19dc3d182
adellafrattina <adellafrattina@studenti.uninsubria.it>
2025-09-05 14:41:41 +0200

    Added compare operation

--- examples/test/Main.cpp
+++ examples/test/Main.cpp
@@ -8,7 +8,7 @@ int main() {
 	std::cout << " 1. (+)\n";
 	std::cout << " 2. (-)\n";
 	std::cout << " 3. (x)\n";
-	//std::cout << " 4. (compare)\n";
+	std::cout << " 4. (compare)\n";
 
 	std::cout << ">";
 	int in;
@@ -17,7 +17,7 @@ int main() {
 
 	big::Integer n1(0);
 	big::Integer n2(0);
-	if (in >= 1 && in <= 3) {
+	if (in >= 1 && in <= 4) {
 
 		std::cout << "Insert first number: ";
 		std::cin >> n1;
@@ -46,22 +46,22 @@ int main() {
 		case 1: n = n1 + n2; break;
 		case 2: n = n1 - n2; break;
 		case 3: n = n1 * n2; break;
-			//case 4:
+		case 4:
 
-			//	if (n1 == n2)
-			//		std::cout << n1 << " == " << n2 << "\n";
-			//	if (n1 != n2)
-			//		std::cout << n1 << " != " << n2 << "\n";
-			//	if (n1 > n2)
-			//		std::cout << n1 << " > " << n2 << "\n";
-			//	if (n1 < n2)
-			//		std::cout << n1 << " < " << n2 << "\n";
-			//	if (n1 >= n2)
-			//		std::cout << n1 << " >= " << n2 << "\n";
-			//	if (n1 <= n2)
-			//		std::cout << n1 << " <= " << n2 << "\n";
+			if (n1 == n2)
+				std::cout << n1 << " == " << n2 << "\n";
+			if (n1 != n2)
+				std::cout << n1 << " != " << n2 << "\n";
+			if (n1 > n2)
+				std::cout << n1 << " > " << n2 << "\n";
+			if (n1 < n2)
+				std::cout << n1 << " < " << n2 << "\n";
+			if (n1 >= n2)
+				std::cout << n1 << " >= " << n2 << "\n";
+			if (n1 <= n2)
+				std::cout << n1 << " <= " << n2 << "\n";
 
-			//	return 0;
+			return 0;
 
 		default:
 			std::cout << "Invalid operation\n";
